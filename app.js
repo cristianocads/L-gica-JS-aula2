275a1bcd14732dc9c5d4e9ec1c4e84a578f07d9e
Cristiano Aguiar <cristianocads@gmail.com>
2024-11-27 12:12:00 -0300

    começo

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-let titulo = document.querySelector('h1')
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha o número secreto entre 1 e 10';
