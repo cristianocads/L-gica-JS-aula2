ded21a45f9f92368ada86623fe7466a79bf450a5
Cristiano Aguiar <cristianocads@gmail.com>
2024-11-27 21:23:41 -0300

    atualizando

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha o número entre 1 e 10.');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
 }
 
 exibirMensagemInicial();
